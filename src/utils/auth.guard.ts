b12b56eaabb3920379900264f57885bcfbd67be4
Zapmal <mzxgd16@gmail.com>
2021-05-28 17:54:19 -0400

    fix: remove things from previous auth strategy

--- src/utils/auth.guard.ts
+++ src/utils/auth.guard.ts
@@ -27,20 +27,4 @@ export class AuthGuard implements CanActivate {
 
     return true;
   }
-
-  validateJwt(request: Request & { user: Record<string, unknown> }) {
-    const authHeader = request.headers.authorization;
-    const SECRET = process.env.JWT_SECRET;
-
-    if (!authHeader || authHeader.startsWith('Bearer ')) {
-      return false;
-    }
-
-    const token = authHeader.split(' ')[1];
-    const user = verify(token, SECRET) as Record<string, unknown>;
-
-    request.user = user;
-
-    return true;
-  }
 }

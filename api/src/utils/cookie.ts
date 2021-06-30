a216cfb5906e2baaa42287dff085533955a637c2
Zapmal <mzxgd16@gmail.com>
2021-06-30 16:50:37 -0400

    fix: set cookie expiry time to ~2 months

--- api/src/utils/cookie.ts
+++ api/src/utils/cookie.ts
@@ -1,9 +1,13 @@
 import { CookieOptions } from 'express';
 
+/**
+ * The maxAge is approx ~2 months.
+ */
 const options: CookieOptions = {
-  maxAge: 1000 * 3600 * 24 * 30 * 2, // Two months in ms.
+  maxAge: 86400 * 90,
   secure: false,
   httpOnly: true,
+  sameSite: 'lax',
 };
 
 export { options as cookieOptions };
